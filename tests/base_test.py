c806e67567c98d59efc311c9a844ca219a83e7fc
Mirage Chung <mirage7714@gmail.com>
2025-01-10 16:23:06 +0800

    Add webdriver auto download function before starting test

--- tests/base_test.py
+++ tests/base_test.py
@@ -2,18 +2,20 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
-
+import chromedriver_autoinstaller
 
 class BaseTest(unittest.TestCase):
 
     def setUp(self):
+        p = chromedriver_autoinstaller.install()
         options = Options()
         options.add_argument('disable-infobars')
         options.add_argument("--disable-extensions")
         options.add_argument('--disable-gpu')
-        s = Service('chromedriver.exe')
+        s = Service(p)
         self.driver = webdriver.Chrome(service=s)
         self.driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
 
     def tearDown(self):
         self.driver.close()
+
